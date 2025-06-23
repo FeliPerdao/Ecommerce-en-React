dc655634c679b4771f54d15e50035c90ea8ca96d
FeliPerdao <f.perdao@gmail.com>
2025-06-23 14:50:59 -0300

    feat: Agrega paginación al ProductList

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Productos from "./Productos";
 import "./styleProductos.css";
 import { useProducts } from "../context/ProductsContext";
+import Pagination from "react-bootstrap/Pagination";
 
 const ProductList = () => {
   const { busqueda, setBusqueda, productosRenderizados } = useProducts();
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 4;
+  const indexOfLast = currentPage * itemsPerPage;
+  const indexOfFirst = indexOfLast - itemsPerPage;
+  const currentProducts = productosRenderizados.slice(
+    indexOfFirst,
+    indexOfLast
+  );
+  const totalPages = Math.ceil(productosRenderizados.length / itemsPerPage);
 
   if (
     !Array.isArray(productosRenderizados) ||
     productosRenderizados.length === 0
   )
-    return <p>No hay productos disponibles en la categoría seleccionada.</p>;
+    return (
+      <>
+        <h2>Galería de Productos</h2>
+        <p>No hay productos disponibles en la categoría seleccionada.</p>;
+      </>
+    );
 
   return (
     <>
@@ -28,9 +44,32 @@ const ProductList = () => {
           justifyContent: "space-evenly",
         }}
       >
-        {productosRenderizados.map((producto) => (
+        {currentProducts.map((producto) => (
           <Productos key={producto.id} producto={producto} />
         ))}
+      </div>
+      <div className="paginador-custom">
+        <button
+          onClick={() => setCurrentPage((p) => Math.max(p - 1, 1))}
+          disabled={currentPage === 1}
+        >
+          {"<"}
+        </button>
+        {Array.from({ length: totalPages }, (_, i) => (
+          <button
+            key={i + 1}
+            className={i + 1 === currentPage ? "activo" : ""}
+            onClick={() => setCurrentPage(i + 1)}
+          >
+            {i + 1}
+          </button>
+        ))}
+        <button
+          onClick={() => setCurrentPage((p) => Math.min(p + 1, totalPages))}
+          disabled={currentPage === totalPages}
+        >
+          {">"}
+        </button>
       </div>
     </>
   );
